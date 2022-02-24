e040e9fda36666cb5a10ec50ef838bf775c24651
Your Name <you@example.com>
2022-02-24 21:52:05 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import {ButtonGroup, CardContent, CardMedia, Typography} from "@mui/material";
+import {Button} from "@mui/material";
+import {Card} from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <div className="header">
+              <div className="nav-menu">
+                  <ButtonGroup variant="contained" aria-label="outlined primary button group">
+                      <Button>HOMEPAGE</Button>
+                      <Button>MOVIES</Button>
+                      <Button>CAST</Button>
+                  </ButtonGroup>
+              </div>
+          </div>
+          <div>
+              <MediaCard/>
+          </div>
+
+      </div>
   );
+}
+
+function MediaCard() {
+    return (
+        <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+                component="img"
+                height="140"
+                image="/static/images/cards/contemplative-reptile.jpg"
+                alt="green iguana"
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    Lizard
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                    species, ranging across all continents except Antarctica
+                </Typography>
+            </CardContent>
+        </Card>
+    );
 }
 
 export default App;
